66f508554af6821375a83696fb5240eb363f5290
“[Aditi <19bcs2779@gmail.com>
2021-04-08 22:10:39 +0530

    created react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +35,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
